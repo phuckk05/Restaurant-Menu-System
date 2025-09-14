33107f94db77cafa08deaa5f866b7d446bd03085
phuckk05 <phuckk2101@gmail.com>
2025-09-14 10:48:54 +0700

    Setup idea

---
+++ BUS_restaurant_menu_system/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS_restaurant_menu_system
+{
+    public class Class1
+    {
+    }
+}
---
+++ ET_restaurant_menu_system/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET_restaurant_menu_system
+{
+    public class Class1
+    {
+    }
+}
